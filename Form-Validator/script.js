f5ecac2200d5dca8384a41cd22676c1ff46211a5
olivercastroteran <olivercastroteran@gmail.com>
2020-02-15 22:40:39 -0400

    JS file config

---
+++ Form-Validator/script.js
@@ -0,0 +1,81 @@
+// DOM Elements
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+// Event Listeners
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  checkPasswordsMatch(password, password2);
+});
+
+// Check required
+const checkRequired = inputArr => {
+  inputArr.forEach(input => {
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+};
+
+// Check email is valid
+const checkEmail = input => {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value)) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email is not valid');
+  }
+};
+
+// Get Field Name
+const getFieldName = input => {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+};
+
+//  Show input error message
+const showError = (input, message) => {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
+  const small = formControl.querySelector('small');
+  small.innerText = message;
+};
+
+// Show Success Outline
+const showSuccess = input => {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
+};
+
+// Check Input Length
+const checkLength = (input, min, max) => {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be les than ${max} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
+};
+
+// check passwords match
+const checkPasswordsMatch = (input1, input2) => {
+  if (input1.value !== input2.value) {
+    showError(input2, 'Passwords do not match');
+  }
+};
