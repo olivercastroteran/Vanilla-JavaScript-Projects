7ceded67adfff7e894865f4de1ca09de64804b7a
olivercastroteran <olivercastroteran@gmail.com>
2020-02-19 11:49:36 -0400

    Menu Slider & Modal JS integration

---
+++ Menu-Slider-and-Modal/script.js
@@ -0,0 +1,21 @@
+// DOM Elements
+const toggle = document.getElementById('toggle');
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+const modal = document.getElementById('modal');
+
+// Toggle nav
+toggle.addEventListener('click', () =>
+  document.body.classList.toggle('show-nav')
+);
+
+// Show Modal
+open.addEventListener('click', () => modal.classList.add('show-modal'));
+
+// Hide Modal
+close.addEventListener('click', () => modal.classList.remove('show-modal'));
+
+// Hide modal from ooutside click
+window.addEventListener('click', e =>
+  e.target == modal ? modal.classList.remove('show-modal') : false
+);
