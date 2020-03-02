12dfa8f71b6708a4f6d4c76f7db8aa86a8e429f1
olivercastroteran <olivercastroteran@gmail.com>
2020-03-01 20:29:57 -0400

    Expense Tracker - transaction functionality plus local storage implemented

--- Expense-Tracker/script.js
+++ Expense-Tracker/script.js
@@ -7,14 +7,50 @@ const form = document.getElementById('form');
 const text = document.getElementById('text');
 const amount = document.getElementById('amount');
 
-const dummyTransactions = [
-  { id: 1, text: 'Flower', amount: -20 },
-  { id: 2, text: 'Salary', amount: 300 },
-  { id: 3, text: 'Book', amount: -10 },
-  { id: 4, text: 'Camera', amount: 150 }
-];
+// const dummyTransactions = [
+//   { id: 1, text: 'Flower', amount: -20 },
+//   { id: 2, text: 'Salary', amount: 300 },
+//   { id: 3, text: 'Book', amount: -10 },
+//   { id: 4, text: 'Camera', amount: 150 }
+// ];
 
-let transactions = dummyTransactions;
+const localStorageTransactions = JSON.parse(
+  localStorage.getItem('transactions')
+);
+
+let transactions =
+  localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
+
+// Add transaction
+function addTransaction(e) {
+  e.preventDefault();
+
+  if (text.value.trim() === '' || amount.value.trim() === '') {
+    Alert('add a text and amount');
+  } else {
+    const transaction = {
+      id: generateID(),
+      text: text.value,
+      amount: +amount.value
+    };
+    // Push transaction into array
+    transactions.push(transaction);
+    // Update DOM
+    addTransactionDOM(transaction);
+    // Update our values
+    updateValues();
+    // Update local storage
+    updateLocalStorage();
+    // Clear inputs
+    text.value = '';
+    amount.value = '';
+  }
+}
+
+// Generate random id
+function generateID() {
+  return Math.floor(Math.random() * 100000000);
+}
 
 // Add transactions to DOM list
 function addTransactionDOM(transaction) {
@@ -24,10 +60,13 @@ function addTransactionDOM(transaction) {
 
   // Add class based on value
   item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+
   item.innerHTML = `
     ${transaction.text} <span>${sign}${Math.abs(
     transaction.amount
-  )}</span> <button class="delete-btn">x</button>
+  )}</span> <button class="delete-btn" onclick="removeTransaction(${
+    transaction.id
+  })">x</button>
   `;
 
   list.appendChild(item);
@@ -48,6 +87,20 @@ function updateValues() {
   money_minus.innerText = `$${expense}`;
 }
 
+// Remove transaction by id
+function removeTransaction(id) {
+  transactions = transactions.filter(transaction => transaction.id !== id);
+
+  updateLocalStorage();
+
+  init();
+}
+
+// Update local storage transactions
+function updateLocalStorage() {
+  localStorage.setItem('transactions', JSON.stringify(transactions));
+}
+
 // Init app
 function init() {
   list.innerHTML = '';
@@ -56,3 +109,6 @@ function init() {
 }
 
 init();
+
+// Event Listeners
+form.addEventListener('submit', addTransaction);
